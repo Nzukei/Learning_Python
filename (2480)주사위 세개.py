a23b77c3b7d2ad0969379a3ea19a2473991425ce
N図形 <53482136+Nzukei@users.noreply.github.com>
2022-06-17 06:34:04 +0900

    Update (2480)주사위 세개.py

--- (2480)주사위 세개.py
+++ (2480)주사위 세개.py
@@ -1,1 +1,14 @@
+A, B, C = map(int, input().split())
+if A == B == C:
+    print(10000 + A * 1000)
 
+elif A != B != C and A != C:
+    print(max(A, B, C) * 100)
+
+else:
+    if A == B != C:
+        print(1000 + A * 100)
+    elif A == C != B:
+        print(1000 + A * 100)
+    else:
+        print(1000 + B * 100)
