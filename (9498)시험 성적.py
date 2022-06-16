55571030cd41e3434c5eab37b8cc4cda11e0704e
N図形 <53482136+Nzukei@users.noreply.github.com>
2022-06-17 06:29:08 +0900

    Create (9498)시험 성적.py

---
+++ (9498)시험 성적.py
@@ -0,0 +1,11 @@
+tensu = int(input())
+if 90 <= tensu:
+    print('A')
+elif 80 <= tensu:
+    print('B')
+elif 70 <= tensu:
+    print('C')
+elif 60 <= tensu:
+    print('D')
+else:
+    print('F')
