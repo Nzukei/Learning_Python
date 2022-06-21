51082c0d22e7b8700e21f60e327a71ff8ba00aff
N図形 <53482136+Nzukei@users.noreply.github.com>
2022-06-21 15:44:40 +0900

    Create (2562)최댓값.py

---
+++ (2562)최댓값.py
@@ -0,0 +1,22 @@
+import sys
+
+number = [int(sys.stdin.readline()) for _ in range(9)] # 리스트 컴프리헨션
+
+print(max(number))
+print(number.index(max(number)) + 1)
+
+# if ver.
+
+# import sys
+#
+# big = int(0)
+# pos = int(0)
+#
+# for i in range(0, 9):
+#     push = int(sys.stdin.readline())
+#     if big < push:
+#         big = push
+#         pos = i + 1
+#
+# print(big)
+# print(pos)
